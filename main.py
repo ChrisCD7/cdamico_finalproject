34cc0f3b0deaaea57a3587250b7eb18ce8873a01
ChrisCD7 <127146462+ChrisCD7@users.noreply.github.com>
2023-04-27 09:28:49 -0700

    requests class

--- main.py
+++ main.py
@@ -18,13 +18,21 @@ from settings import *
 # from https://automatetheboringstuff.com/2e/chapter12/
 from bs4 import BeautifulSoup
 
-pg.init()
-pg.mixer.init()
 
-screen = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption("Stat Checker")
-clock = pg.time.Clock()
+res = requests.get('https://www.baseball-reference.com')
+type(res)
+res.raise_for_status()
+playFile = open('bsblref.txt', 'wb')
+for chunk in res.iter_content(100000):
+    playFile.write(chunk)
+playFile.close()
 
+class Requests('requests.models.Response'):
+    res.status_code == requests.codes.ok
+    True
+    len(res.text)
+    178981
+    print(res.text[:250])
 
 url = "https://www.baseball-reference.com/leagues/MLB/2021.shtml"
 response = requests.get(url)
