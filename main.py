868f29cf182ad2ad4206f4b740a10bd72cfaf7ba
ChrisCD7 <127146462+ChrisCD7@users.noreply.github.com>
2023-05-02 10:39:27 -0700

    reset

--- main.py
+++ main.py
@@ -20,29 +20,20 @@ from settings import *
 from bs4 import BeautifulSoup
 
 
-res = requests.get('https://www.baseball-reference.com')
-type(res)
+URL = 'https://www.baseball-reference.com'
+
+# get info from website
+res = requests.get(URL)
 res.raise_for_status()
-playFile = open('bsblref.txt', 'wb')
-for chunk in res.iter_content(100000):
-    playFile.write(chunk)
-playFile.close()
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+# print(type(soup))
+# print(soup)
+elem = soup.select('div')
+# print(len(elem))
+print(str(elem))
+# dump into excel
 
-
-res = requests.get('https://www.baseball-reference.com')
-res.raise_for_status()
-noStarchSoup = bs4.BeautifulSoup(res.text, 'html.parser')
-type(noStarchSoup)
-
-class Requests('requests.models.Response'):
-    res.status_code == requests.codes.ok
-    True
-    len(res.text)
-    178981
-    print(res.text[:250])
-
-url = "https://www.baseball-reference.com/leagues/MLB/2021.shtml"
-response = requests.get(url)
+response = requests.get(URL)
 soup = BeautifulSoup(response.content, "html.parser")
 table = soup.find("table", {"id": "team_pitching"})
 player_name = table.find("tbody").find("tr").find("td", {"data-stat": "player"}).find("a").text
