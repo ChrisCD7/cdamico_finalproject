1e2e119eff935e605fc1426f286376a21d6a9f73
ChrisCD7 <127146462+ChrisCD7@users.noreply.github.com>
2023-05-03 11:29:07 -0700

    reformat for window

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Goals:
 Create screen scraper that...
 finds player stats (mlb),
 averages them into career stats, 
-has a interface through pygame
+has a interface through pg
 '''
 
 
@@ -18,7 +18,6 @@ from settings import *
 from bs4 import BeautifulSoup
 
 
-
 # Initialize pygame and set up the window
 pg.init()
 screen = pg.display.set_mode((800, 600))
@@ -29,7 +28,19 @@ font = pg.font.SysFont(None, 30)
 text_color = pg.Color("black")
 
 # Ask for player's name input
-player_name = input("Enter an MLB player's name: ")
+player_name = ""
+while not player_name:
+    event = pg.event.poll()
+    if event.type == pg.QUIT:
+        pg.quit()
+        quit()
+    elif event.type == pg.KEYDOWN:
+        if event.key == pg.K_RETURN:
+            break
+        elif event.key == pg.K_BACKSPACE:
+            player_name = player_name[:-1]
+        else:
+            player_name += event.unicode
 
 # Create the URL to search for the player's stats
 url = "https://www.baseball-reference.com/search/search.fcgi?search=" + player_name.replace(" ", "+")
@@ -48,21 +59,48 @@ player_url = "https://www.baseball-reference.com" + player_link
 player_response = requests.get(player_url)
 player_soup = BeautifulSoup(player_response.content, "html.parser")
 
-# Find the player's stats table and print the table headers
+# Find the player's stats table
 stats_table = player_soup.find("table", {"id": "batting_standard"})
+
+# Set up the x and y positions for the text
+x_pos = 100
+y_pos = 100
+
+# Print the table headers
 headers = stats_table.find_all("th")
 for header in headers:
-    print(header.text, end="\t")
+    text_surface = font.render(header.text, True, text_color)
+    screen.blit(text_surface, (x_pos, y_pos))
+    x_pos += 100
 
-print()
+# Reset the x position and increment the y position
+x_pos = 100
+y_pos += 30
 
-# Find the player's stats rows and print the statistics
+# Print the statistics
 stats_rows = stats_table.find("tbody").find_all("tr")
 for row in stats_rows:
     cells = row.find_all("td")
     for cell in cells:
-        print(cell.text, end="\t")
-    print()
+        text_surface = font.render(cell.text, True, text_color)
+        screen.blit(text_surface, (x_pos, y_pos))
+        x_pos += 100
+    x_pos = 100
+    y_pos += 30
 
+# Update the display
+pg.display.flip()
+
+# Wait for the user to close the window
+while True:
+    event = pg.event.wait()
+    if event.type == pg.QUIT:
+        pg.quit()
+        quit()
+
+# Initialize pg and set up the window
+pg.init()
+screen = pg.display.set_mode((800, 600))
+pg.display.set_caption("MLB Player Stats")
 
 
